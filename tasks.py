1f41c878eabfb10333387d57ace58bdce0eba7c4
irom77 <irek.romaniuk@gmail.com>
2018-01-08 15:28:34 -0500

    fix

--- tasks.py
+++ tasks.py
@@ -6,9 +6,13 @@ app = Celery('tasks', broker='pyamqp://guest@localhost//')
 def add(x, y):
     return x + y
 
-import subprocess
+import delegator
 @app.task
 def ping(hostname):
+    """
     (output, error) = subprocess.Popen(['ping', '-c 2', hostname], 
     stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate() # , shell=True
     print(output, error)
+    """
+    c = delegator.run('ping -n 2 ' + hostname, block=False)
+    return c
