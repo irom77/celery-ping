566219878b26bef24cba9fe23937b21884b28770
irom77 <irek.romaniuk@gmail.com>
2018-01-08 14:51:36 -0500

    fix

--- ping.py
+++ ping.py
@@ -2,6 +2,6 @@ import subprocess
 
 def ping(hostname):
     (output, error) = subprocess.Popen('ping ' + hostname, 
-    stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+    stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).communicate()
     print(output, error)
 
